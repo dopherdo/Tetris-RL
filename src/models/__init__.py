3f2b10aa52e4739c23fa7b57ffab0b28e5afff2b
Chris Yeh <dopherdo@users.noreply.github.com>
2025-11-21 19:02:03 -0800

    added a PPO agent

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -2,6 +2,7 @@
 Neural Network Models Module
 """
 from src.models.cnn_policy import TetrisCNN, ActorCriticNetwork, create_policy_network
+from src.models.ppo_agent import PPOAgent
 
-__all__ = ['TetrisCNN', 'ActorCriticNetwork', 'create_policy_network']
+__all__ = ['TetrisCNN', 'ActorCriticNetwork', 'create_policy_network', 'PPOAgent']
 
--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -2,6 +2,7 @@
 Utility Functions Module
 """
 from src.utils.preprocessing import TetrisPreprocessor, create_preprocessor
+from src.utils.rollout_buffer import RolloutBuffer
 
-__all__ = ['TetrisPreprocessor', 'create_preprocessor']
+__all__ = ['TetrisPreprocessor', 'create_preprocessor', 'RolloutBuffer']
 
